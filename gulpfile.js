e63cf43dfb17778043a714498c8a154a48222d72
AlexAvD <alex.aganow@yandex.ru>
2020-08-28 22:02:32 +0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,126 +1,118 @@
-const { src, dest, parallel, series, watch } = require('gulp');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCss = require('gulp-clean-css');
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
-const sourcemap = require('gulp-sourcemaps');
-const changed = require('gulp-changed');
-const imagemin = require('gulp-imagemin');
-const del = require('del');
-const browserSync = require('browser-sync').create();
+const { src, dest, parallel, series, watch } = require("gulp");
+const autoprefixer = require("gulp-autoprefixer");
+const cleancss = require("gulp-clean-css");
+const babel = require("gulp-babel");
+const webpack = require("webpack-stream");
+const rename = require("gulp-rename");
+const uglify = require("gulp-uglify");
+const sass = require("gulp-sass");
+const sourcemap = require("gulp-sourcemaps");
+const imagemin = require("gulp-imagemin");
+const del = require("del");
+const browserSync = require("browser-sync").create();
 
-const paths = {
-	input: './src/',
-	output: './dist/',
-	scss: {
-		src: './src/scss/main.scss',
-		dist: './dist/css/'
-	},
-	js: {
-		src: './src/js/**/*.js',
-		dist: './dist/js/'
-	},
-	html: {
-		src: './src/index.html',
-		dist: './dist/'
-	},
-	img: {
-		src: './src/img/**/*.*',
-		dist: './dist/img/'
-	},
-	fonts: {
-		src: './src/fonts/**/*.*',
-		dist: './dist/fonts/'
-	},
-	watch: {
-		scss: './src/scss/**/*.scss',
-		js: './src/js/**/*.js',
-		html: './src/index.html',
-		img: './src/img/**/*.*',
-		fonts: './src/fonts/**/*.*'
-	}
+const INPUT_PATH = "./src";
+const OUTPUT_PATH = "./build";
+
+const PATHS = {
+  scss: {
+    src: `${INPUT_PATH}/scss/main.scss`,
+    output: {
+      filename: "template.css",
+      dir: `${OUTPUT_PATH}/css/`,
+    },
+  },
+  js: {
+    src: `${INPUT_PATH}/js/**/*.js`,
+    dist: `${OUTPUT_PATH}/js/`,
+    outputFilename: "template.js",
+  },
+  html: {
+    src: `${INPUT_PATH}/index.html`,
+    dist: `${OUTPUT_PATH}`,
+  },
+  img: {
+    src: `${INPUT_PATH}/images/**/*.*`,
+    dist: `${OUTPUT_PATH}/images/`,
+  },
+  fonts: {
+    src: `${INPUT_PATH}/fonts/**/*.*`,
+    dist: `${OUTPUT_PATH}/fonts/`,
+  },
+  watch: {
+    scss: `${INPUT_PATH}/scss/**/*.scss`,
+    js: `${INPUT_PATH}/js/**/*.js`,
+    html: `${INPUT_PATH}/index.html`,
+    img: `${INPUT_PATH}/img/**/*.*`,
+    fonts: `${INPUT_PATH}/fonts/**/*.*`,
+  },
 };
 
 const styles = () => {
-	return src(paths.scss.src)
-		.pipe(changed(paths.scss.dist, { extension: '.css' }))
-		.pipe(sourcemap.init())
-		.pipe(sass().on('error', sass.logError))
-		.pipe(autoprefixer({ cascade: false }))
-		.pipe(cleanCss({ level: 2 }))
-		.pipe(sourcemap.write('./'))
-		.pipe(dest(paths.scss.dist))
-		.pipe(browserSync.stream());
+  return src(PATHS.scss.src)
+    .pipe(sourcemap.init())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(autoprefixer())
+    .pipe(cleancss({ level: 2, format: "beautify" }))
+    .pipe(rename(PATHS.scss.output.filename))
+    .pipe(sourcemap.write("."))
+    .pipe(dest(PATHS.scss.output.dir))
+    .pipe(browserSync.stream());
 };
 
 const scripts = () => {
-	return src(paths.js.src)
-		.pipe(changed(paths.js.dist))
-		.pipe(sourcemap.init())
-		.pipe(babel({ presets: ['@babel/env'] }))
-		.pipe(uglify())
-		.pipe(sourcemap.write('./'))
-		.pipe(dest(paths.js.dist))
-		.pipe(browserSync.stream());
+  return src(PATHS.js.src)
+    .pipe(sourcemap.init())
+    .pipe(
+      webpack({
+        mode: "none",
+        output: {
+          filename: PATHS.js.outputFilename,
+        },
+      })
+    )
+    .pipe(babel({ presets: ["@babel/env"] }))
+    .pipe(sourcemap.write("./"))
+    .pipe(dest(PATHS.js.dist))
+    .pipe(browserSync.stream());
 };
 
 const html = () => {
-	return src(paths.html.src)
-		.pipe(changed(paths.html.dist))
-		.pipe(dest(paths.html.dist))
-		.pipe(browserSync.stream());
+  return src(PATHS.html.src).pipe(dest(PATHS.html.dist)).pipe(browserSync.stream());
 };
 
 const fonts = () => {
-	return src(paths.fonts.src)
-		.pipe(changed(paths.fonts.dist))
-		.pipe(dest(paths.fonts.dist))
-		.pipe(browserSync.stream());
+  return src(PATHS.fonts.src).pipe(dest(PATHS.fonts.dist)).pipe(browserSync.stream());
 };
 
 const images = () => {
-	return src(paths.img.src)
-		.pipe(changed(paths.img.dist))
-		.pipe(imagemin())
-		.pipe(dest(paths.img.dist))
-		.pipe(browserSync.stream());
+  return src(PATHS.img.src).pipe(imagemin()).pipe(dest(PATHS.img.dist)).pipe(browserSync.stream());
 };
 
 const clean = () => {
-	return del(`${paths.output}*`);
-};
-
-const reload = (done) => {
-	browserSync.reload();
-	done();
+  return del(OUTPUT_PATH);
 };
 
 const watchFiles = (done) => {
-	watch(paths.watch.html, html);
-	watch(paths.watch.scss, styles);
-	watch(paths.watch.fonts, fonts);
-	watch(paths.watch.img, images);
-	watch(paths.watch.js, scripts);
+  watch(PATHS.watch.html, html);
+  watch(PATHS.watch.scss, styles);
+  watch(PATHS.watch.fonts, fonts);
+  watch(PATHS.watch.img, images);
+  watch(PATHS.watch.js, scripts);
 
-	done();
+  done();
 };
 
 const server = (done) => {
-	browserSync.init({
-		server: {
-			baseDir: paths.output
-		},
-		notify: false
-	});
+  browserSync.init({
+    server: {
+      baseDir: OUTPUT_PATH,
+    },
+    notify: false,
+  });
 
-	done();
+  done();
 };
 
 exports.clean = clean;
-exports.dev = series(
-	clean,
-	parallel(scripts, html, styles, images, fonts),
-	server,
-	watchFiles
-);
+exports.dev = series(clean, parallel(scripts, html, styles, images, fonts), server, watchFiles);
